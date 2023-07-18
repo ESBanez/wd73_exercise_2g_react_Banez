77f44150e15049cb1597ff9e88cb3aa32cd467a1
Ederick S. Banez <esb.tailor@gmail.com>
2023-07-19 00:23:29 +0800

    added image card 1-16 contents not updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,136 @@
+import Navbar from "./components/Navigation.jsx"
+import StudentCard from "./components/SheinCard.jsx"
+import Scrollaos from "./components/Scroll.jsx"
+
 
 
 function App() {
+  const students = [
+    {
+      id: 1,
+      firstName: 'Black Dress',
+      formerPrice: 'P 300',
+      latestPrice: 'P 100',
+      image: 'https://i.ibb.co/6Z8BHk7/black-dress.jpg'
+    },
+    {
+      id: 2,
+      firstName: 'Black Dress',
+      formerPrice: 'P 300',
+      latestPrice: 'P 100',
+      image: 'https://i.ibb.co/zxzwf48/brown-dress.jpg'
+    },
+    {
+      id: 3,
+      firstName: 'Black Dress',
+      formerPrice: 'P 300',
+      latestPrice: 'P 100',
+      image: 'https://i.ibb.co/t3m71mP/green-dress.jpg'
+    },
+    {
+      id: 1,
+      firstName: 'Black Dress',
+      formerPrice: 'P 300',
+      latestPrice: 'P 100',
+      image: 'https://i.ibb.co/6Z8BHk7/black-dress.jpg'
+    },
+    {
+      id: 2,
+      firstName: 'Black Dress',
+      formerPrice: 'P 300',
+      latestPrice: 'P 100',
+      image: 'https://i.ibb.co/zxzwf48/brown-dress.jpg'
+    },
+    {
+      id: 3,
+      firstName: 'Black Dress',
+      formerPrice: 'P 300',
+      latestPrice: 'P 100',
+      image: 'https://i.ibb.co/t3m71mP/green-dress.jpg'
+    },
+    {
+      id: 1,
+      firstName: 'Black Dress',
+      formerPrice: 'P 300',
+      latestPrice: 'P 100',
+      image: 'https://i.ibb.co/6Z8BHk7/black-dress.jpg'
+    },
+    {
+      id: 2,
+      firstName: 'Black Dress',
+      formerPrice: 'P 300',
+      latestPrice: 'P 100',
+      image: 'https://i.ibb.co/zxzwf48/brown-dress.jpg'
+    },
+    {
+      id: 3,
+      firstName: 'Black Dress',
+      formerPrice: 'P 300',
+      latestPrice: 'P 100',
+      image: 'https://i.ibb.co/t3m71mP/green-dress.jpg'
+    },
+    {
+      id: 1,
+      firstName: 'Black Dress',
+      formerPrice: 'P 300',
+      latestPrice: 'P 100',
+      image: 'https://i.ibb.co/6Z8BHk7/black-dress.jpg'
+    },
+    {
+      id: 2,
+      firstName: 'Black Dress',
+      formerPrice: 'P 300',
+      latestPrice: 'P 100',
+      image: 'https://i.ibb.co/zxzwf48/brown-dress.jpg'
+    },
+    {
+      id: 3,
+      firstName: 'Black Dress',
+      formerPrice: 'P 300',
+      latestPrice: 'P 100',
+      image: 'https://i.ibb.co/t3m71mP/green-dress.jpg'
+    },
+    {
+      id: 1,
+      firstName: 'Black Dress',
+      formerPrice: 'P 300',
+      latestPrice: 'P 100',
+      image: 'https://i.ibb.co/6Z8BHk7/black-dress.jpg'
+    },
+    {
+      id: 2,
+      firstName: 'Black Dress',
+      formerPrice: 'P 300',
+      latestPrice: 'P 100',
+      image: 'https://i.ibb.co/zxzwf48/brown-dress.jpg'
+    },
+    {
+      id: 3,
+      firstName: 'Black Dress',
+      formerPrice: 'P 300',
+      latestPrice: 'P 100',
+      image: 'https://i.ibb.co/t3m71mP/green-dress.jpg'
+    },
+    {
+      id: 3,
+      firstName: 'Black Dress',
+      formerPrice: 'P 300',
+      latestPrice: 'P 100',
+      image: 'https://i.ibb.co/t3m71mP/green-dress.jpg'
+    }
+  ];
+
+  const studentCardList = students.map(
+    (student) => <StudentCard key={student.id} firstName={student.firstName} formerPrice={student.formerPrice} latestPrice={student.latestPrice} image={student.image} />
+  );
 
   return (
-    <>
-      <h1>Hello World</h1>
+   <>
+      <Navbar/>
+      <h1 className="text-warning container">Welcome to the worlds largest brand</h1>
+      <div className="container d-flex flex-wrap">
+        {studentCardList} 
+      </div>
     </>
   )
 }
---
+++ src/components/Scroll.jsx
@@ -0,0 +1,10 @@
+const Scrollaos = () => {
+    return (
+      <div>
+        <h1 data-aos="fade-up">Animate On Scroll</h1>
+        <p data-aos="zoom-in">Scroll down to see the animations!</p>
+      </div>
+    );
+  };
+  
+  export default Scrollaos;
